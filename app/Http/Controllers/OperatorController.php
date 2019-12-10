152721fd1bb942b083b9c95afe5d58247a15b9a2
AldhyMahardika <aldhymahardika@gmail.com>
2019-12-10 20:28:38 +0700

    uji coba operator

--- app/Http/Controllers/OperatorController.php
+++ app/Http/Controllers/OperatorController.php
@@ -18,36 +18,37 @@ class OperatorController extends Controller
     }
 
     public function store_doc(Request $request){
-    	$this->validate($request, [
-    		'name' => 'required',
-    		'name_doc'=> 'required',
-			'ktp' => 'required|file|image|mimes:jpeg,png,jpg|max:2048',
-			'ijazah' => 'required|file|image|mimes:jpeg,png,jpg|max:2048',
-			'akte' => 'required|file|image|mimes:jpeg,png,jpg|max:2048',
-			'keterangan' => 'required',
-			'instansi' => 'required',
-		]);
 
+        $operator = new \App\Operator;
+        $operator->name = $request->name;
+        $operator->name_doc = $request->name_doc;
         $ktp = $request->file('file');
         $ijazah = $request->file('file');
         $akte = $request->file('file');
-        $name_ktp = $file->getClientOriginalName();
-        $name_ijazah = $file->getClientOriginalName();
-        $name_akte = $file->getClientOriginalName();
+        $name_ktp = $ktp->getClientOriginalExtension();
+        $name_ijazah = $ijazah->getClientOriginalExtension();
+        $name_akte = $akte->getClientOriginalExtension();
         $tujuan = 'upload';
         $ktp->move($tujuan, $name_ktp);
         $ijazah->move($tujuan, $name_ijazah);
         $akte->move($tujuan, $name_akte);
 
-        Operator::create([
-        	'name' => $request->name,
-        	'name_doc' => $request->name_doc,
-        	'ktp' => $name_ktp,
-        	'ijazah' => $name_ijazah,
-        	'akte' => $name_akte,
-        	'keterangan' => $request->keterangan,
-        	'instansi' => $request->instansi
-        ]);
+        // Operator::create([
+        // 	'name' => $request->name,
+        // 	'name_doc' => $request->name_doc,
+        // 	'ktp' => $name_ktp,
+        // 	'ijazah' => $name_ijazah,
+        // 	'akte' => $name_akte,
+        // 	'keterangan' => $request->keterangan,
+        // 	'instansi' => $request->instansi
+        // ]);
+
+        $operator->ktp = $name_ktp;
+        $operator->ijazah = $name_ijazah;
+        $operator->akte = $name_akte;
+        $operator->keterangan = $request->keterangan;
+        $operator->instansi = $request->instansi;
+        $operator->save();
 
         return redirect('operator.data_doc');
     }
